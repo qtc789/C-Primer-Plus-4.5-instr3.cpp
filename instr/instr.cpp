aec4eb5ba4f767ab071bb233fb02086c490c34bf
qtc789 <qiantongci@126.com>
2024-11-15 23:44:09 +0800

    Add files via upload

---
+++ instr/instr.cpp
@@ -0,0 +1,22 @@
+﻿ // instr.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    const int ArSize = 20;
+    char name[ArSize];
+    char dessert[ArSize];
+
+    cout << "Enter your name;\n";
+    cin.get(name, ArSize).get();
+    cout << "Enter your favorite dessert;\n";
+    cin.get(dessert, ArSize);
+    cout << "I have some delicious " << dessert;
+    cout << " for you," << name << " .\n";
+
+    return 0;
+}
+
